88a3d52067b91830e7fdf7e5cd32f3d12c94c2d2
freetimezp <kraftukrnet@ukr.net>
2022-10-13 19:53:56 +0300

    12. js-halloween-01. Animation use ScrollReveal.

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,9 +67,21 @@ window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true,
+});
 
-
-
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`);
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800});
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800});
+sr.reveal(`.home__img, .footer__copy`, {origin: 'bottom', delay: 1200});
+sr.reveal(`.category__card, .items__card`, {interval: 100});
+sr.reveal(`.about__data, .footer__tree-1`, {origin: 'right'});
+sr.reveal(`.party__images, .party__data, .footer__tree-2`, {origin: 'left'});
 
 
 
